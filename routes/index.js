7b386f7ad0cdb85b7c7e3e65a2651e159df454d2
Shehan Kulathilake <shehanhere@gmail.com>
2025-04-03 22:59:00 +0530

    testing redirects

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,18 @@ const router = express.Router();
 
 // Serve the index.html file for the root route
 router.get('/', (req, res) => {
+
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+router.get('/redirect', (req,res)=>{
   res.cookie('secure_cookie','hello',{
     httpOnly: true,
     secure: true,
     sameSite: 'lax',
     path:'/'
   })
-  res.sendFile(path.join(__dirname, '../views/index.html'));
-});
+  res.redirect("/");
+})
 
 module.exports = router;
